39abe1e4f13a515c296a5d108abff35f02f6eceb
schon <schon.jesper@gmail.com>
2023-09-29 13:12:12 +0200

    La till all funktionalitet i TaskController Uppdaterade HTML och CSS

--- demo/src/main/java/com/example/demo/TaskController.java
+++ demo/src/main/java/com/example/demo/TaskController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class TaskController {
@@ -21,9 +24,29 @@ public class TaskController {
     return "index";
   }
 
+  @GetMapping("/edit/{id}")
+  public String editTaskForm(@PathVariable Long id, Model model) {
+    Optional<Task> taskOptional = taskRepository.findById(id);
+    taskOptional.ifPresent(task -> model.addAttribute("task", task));
+    return "edit";
+  }
+
+  @PostMapping("/update")
+  public String updateTask(@ModelAttribute Task task) {
+    taskRepository.save(task);
+    return "redirect:/";
+  }
+
+  @PostMapping("/delete/{id}")
+  public String deleteTask(@PathVariable Long id) {
+    taskRepository.deleteById(id);
+    return "redirect:/";
+  }
+
   @PostMapping("/add")
   public String addTask(@ModelAttribute Task task) {
     taskRepository.save(task);
     return "redirect:/";
   }
 }
+
