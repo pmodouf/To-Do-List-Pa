ba39fb54c50e906bfbd09e074951302ebea003b1
schon <schon.jesper@gmail.com>
2023-09-29 10:57:28 +0200

    Skapade Entity klass, Task, la till variabler och constructor, getters & setters.

--- demo/src/main/java/com/example/demo/Task.java
+++ demo/src/main/java/com/example/demo/Task.java
@@ -1,4 +1,67 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Task {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  private String deadline;
+  private boolean status;
+
+  public Task() {}
+
+  public Task(Long id, String name, String description, String deadline, boolean status) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.deadline = deadline;
+    this.status = status;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDeadline() {
+    return deadline;
+  }
+
+  public void setDeadline(String deadline) {
+    this.deadline = deadline;
+  }
+
+  public boolean isStatus() {
+    return status;
+  }
+
+  public void setStatus(boolean status) {
+    this.status = status;
+  }
 }
